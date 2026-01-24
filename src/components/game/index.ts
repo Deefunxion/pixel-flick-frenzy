df1f393353c1848b743a735a3dae89c77ab0b7fc
Deefunxion <deefunxion@example.com>
2026-01-24 17:28:17 +0200

    refactor(Game): add game components barrel

---
+++ src/components/game/index.ts
@@ -0,0 +1,5 @@
+export { default as Game } from '../Game';
+export { AudioWarningToast } from './AudioWarningToast';
+export { useGameAudio } from './useGameAudio';
+export { useGameInput } from './useGameInput';
+export { useGameLoop } from './useGameLoop';
